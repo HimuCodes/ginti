e9516eecb168ea95e4d414867f6980b0f6ffbbf9
Himanshu Tiwari <himanshu.tiwari.17@outlook.com>
2026-02-16 14:39:55 +0530

    used bufio and its reader func to replace prev logic and avoid rune error

--- main.go
+++ main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"os"
 	"strings"
 	"unicode"
-	"unicode/utf8"
 )
 
 func main() {
-	filename := "words.txt"
+	filename := "lots_of_words.txt"
 
 	log.SetFlags(0)
 
@@ -27,37 +27,20 @@ func CountWordsInFile(file *os.File) int {
 	wordCount := 0
 	isInsideWord := false
 
-	const bufferSize = 1
-	buffer := make([]byte, bufferSize)
+	_ = isInsideWord
 
-	leftover := []byte{}
-
+	reader := bufio.NewReader(file)
 	for {
-		size, err := file.Read(buffer)
+		r, _, err := reader.ReadRune()
 		if err != nil {
 			break
 		}
 
-		subBuffer := append(leftover, buffer[:size]...)
-
-		for len(subBuffer) > 0 {
-
-			r, rsize := utf8.DecodeRune(subBuffer)
-
-			if r == utf8.RuneError {
-				break
-			}
-			subBuffer = subBuffer[rsize:]
-
-			if !unicode.IsSpace(r) && !isInsideWord {
-				wordCount++
-			}
-
-			isInsideWord = !unicode.IsSpace(r)
+		if !unicode.IsSpace(r) && !isInsideWord {
+			wordCount++
 		}
 
-		leftover = nil
-		leftover = append(leftover, subBuffer...)
+		isInsideWord = !unicode.IsSpace(r)
 	}
 
 	return wordCount
